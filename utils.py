1f200d257d360b2fb7fdf141a9c70e6f0c99ade2
annaruban <anna14196@ukr.net>
2025-02-27 21:26:34 +0100

    fix for '/n' in f string

--- utils.py
+++ utils.py
@@ -2,5 +2,6 @@ def validate_top_args(args: list[str]) -> None:
     constants = {"keyword", "category", "year",
                  "language", "length", "actor"}
     if not set(args).issubset(constants):
+        s = {'\n'.join(constants)}
         raise Exception(f"""Please search by the following values:
-{'\n'.join(constants)}""")
+{s}""")
